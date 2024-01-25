2eaf8d707cb00db7df2fd1b7bb529ec0873c0e26
22p31a0507 <119730122+22p31a0507@users.noreply.github.com>
2024-01-25 18:47:21 +0530

    import java.util.Scanner; public class Area{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         System.out.printf("%d %d",x*x,4*x);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Area{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.printf("%d %d",x*x,4*x);
+    }
+}
