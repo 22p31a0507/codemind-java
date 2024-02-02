e8aa161c72a40890ef0659f289efe6ab7cbf7887
22p31a0507 <119730122+22p31a0507@users.noreply.github.com>
2024-02-02 10:50:12 +0530

    import java.util.Scanner; public class Kmp{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         double b = 0.27778*a;         System.out.printf("%.2f",b);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Kmp{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = 0.27778*a;
+        System.out.printf("%.2f",b);
+    }
+}
