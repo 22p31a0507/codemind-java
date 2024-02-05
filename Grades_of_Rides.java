8f74824ae0990d2eec3a83c9215ebad8403de136
22p31a0507 <119730122+22p31a0507@users.noreply.github.com>
2024-02-05 11:56:28 +0530

    import java.util.Scanner; public class Grade{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int Hurl = sc.nextInt();         int Spin = sc.nextInt();         int Speed = sc.nextInt();         if(Hurl > 50 && Spin > 60 && Speed > 100)         {             System.out.println(10);         }         else if(Hurl > 50 && Spin > 60 && Speed < 100)         {             System.out.println(9);         }         else if(Hurl < 50 && Spin > 60 && Speed > 100)         {             System.out.println(8);         }         else if(Hurl > 50 && Spin < 60 && Speed > 100)         {             System.out.println(7);         }         else if(Hurl > 50 && Spin < 60 && Speed < 100)         {             System.out.println(6);         }         else if(Hurl < 50 && Spin > 60 && Speed < 100)         {             System.out.println(6);         }         else if(Hurl < 50 && Spin < 60 && Speed > 100)         {             System.out.println(6);         }         else         {             System.out.println(5);         }
        }
    }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Grade{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int Hurl = sc.nextInt();
+        int Spin = sc.nextInt();
+        int Speed = sc.nextInt();
+        if(Hurl > 50 && Spin > 60 && Speed > 100)
+        {
+            System.out.println(10);
+        }
+        else if(Hurl > 50 && Spin > 60 && Speed < 100)
+        {
+            System.out.println(9);
+        }
+        else if(Hurl < 50 && Spin > 60 && Speed > 100)
+        {
+            System.out.println(8);
+        }
+        else if(Hurl > 50 && Spin < 60 && Speed > 100)
+        {
+            System.out.println(7);
+        }
+        else if(Hurl > 50 && Spin < 60 && Speed < 100)
+        {
+            System.out.println(6);
+        }
+        else if(Hurl < 50 && Spin > 60 && Speed < 100)
+        {
+            System.out.println(6);
+        }
+        else if(Hurl < 50 && Spin < 60 && Speed > 100)
+        {
+            System.out.println(6);
+        }
+        else
+        {
+            System.out.println(5);
+        }
+        
+    }
+}
