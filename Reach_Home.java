2a410f1715794ba7bdf4f1a0dd3637ce210d85bf
22p31a0507 <119730122+22p31a0507@users.noreply.github.com>
2024-02-07 10:14:26 +0530

    import java.util.Scanner; public class Reach{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         if(5*a >= b){             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Reach_Home.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Reach{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(5*a >= b){
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
