833ae85509bab84e16575b32eb8ac3f9ee709848
22p31a0507 <119730122+22p31a0507@users.noreply.github.com>
2024-02-16 10:48:31 +0530

    import java.util.Scanner; public class Prime{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int c = 0;         for(int i=2;i<=a/2;i++)         {             if(a%i == 0)             {                 c++;                 break;             }         }         if(c == 0)         {             System.out.println("Prime");         }         else         {             System.out.println("Not Prime");         }     } }

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Prime{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = 0;
+        for(int i=2;i<=a/2;i++)
+        {
+            if(a%i == 0)
+            {
+                c++;
+                break;
+            }
+        }
+        if(c == 0)
+        {
+            System.out.println("Prime");
+        }
+        else
+        {
+            System.out.println("Not Prime");
+        }
+    }
+}
